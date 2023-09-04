6433a2f509da3a4dbe21201b9c614c8a3c9becce
malit mohamut <60866464+mawlid1431@users.noreply.github.com>
2023-09-04 16:03:49 +0300

    Final push for the reverse words task

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// Good Luck 💪🏾
+function reverseWords(sentence) {
+  // Split the sentence into words using space as a delimiter
+  const words = sentence.split(' ');
+
+  // Reverse the array of words
+  const reversedWords = words.reverse();
+
+  // Join the reversed words back into a sentence
+  const reversedSentence = reversedWords.join(' ');
+
+  return reversedSentence;
+}
+
+// Example usages:
+console.log(reverseWords("fikrcamp bootcamp")); // Output: "bootcamp fikrcamp"
+console.log(reverseWords("hello there!")); // Output: "there! hello"
+console.log(reverseWords("welcome to my city")); // Output: "city my to welcome"
+
